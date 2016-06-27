6957ade417841687dc92ba38b63a16a6759b87ab
Jakub Steiner <jimmac@gmail.com>
2016-06-27 19:11:35 +0200

    Automated commit at 2016-06-27 17:11:35 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all-8ebb537d.js
+++ javascripts/all-8ebb537d.js
@@ -18,10 +18,9 @@
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
+        offset: 80
     })
-
+    
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
         $('.navbar-toggle:visible').click();
